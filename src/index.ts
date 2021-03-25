d1b2cac9253f6db2313762ab9ef0b6670de2b06c
Foreinyel <Foreinyel@163.com>
2021-03-25 18:33:40 +0800

    jvs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import chalk from "chalk";
+import program from "./commands";
 
 export default () => {
   console.log(chalk.green("Hello Jarvis!"));
+  console.info(program.parse(process.argv).opts());
 };
